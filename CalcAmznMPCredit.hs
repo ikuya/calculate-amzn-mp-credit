9150315148c00757e9f39badbad57e0f44a45e42
ikuya <if@ifma.local>
2015-01-04 22:11:43 +0900

    [clean] Refactoring

--- CalcAmznMPCredit.hs
+++ CalcAmznMPCredit.hs
@@ -2,13 +2,17 @@ import System.Environment (getArgs)
 import Control.Monad (when)
 import Text.Read (readMaybe)
 
+--------------------------------------------------
+
 data Category = Book | CD deriving (Show, Eq)
 
 type Price = Int
 type Credit = Int
 
-calcCreditForCategory :: Category -> Price -> Credit
-calcCreditForCategory category price
+--------------------------------------------------
+
+calculateCredit :: Category -> Price -> Credit
+calculateCredit category price
   = price + (shippingCharge category)           -- Item price + Shipping chage
           - (variableClosingFee category)       -- Variable closing fee
           - (round $ fromIntegral price * 0.15) -- Referral fee
@@ -21,15 +25,19 @@ calcCreditForCategory category price
       | category == Book = 60
       | category == CD   = 140
 
-calcCredit :: Price -> (Credit, Credit)
-calcCredit price = (calcCreditForCategory Book price,
-                    calcCreditForCategory CD price)
+creditPair :: Price -> (Credit, Credit)
+creditPair price = (calculateCredit Book price
+                   ,calculateCredit CD price)
 
+--------------------------------------------------
+
+main :: IO ()
 main = do
   args <- getArgs
   when (not $ null args) $ do
     case readMaybe $ head args of
-      Just price -> let creditPair = calcCredit price
-                    in putStrLn $ "Book:" ++ (show $ fst creditPair) ++
-                                  " CD/DVD:" ++ (show $ snd creditPair)
+      Just price -> let credits = creditPair price
+                    in do
+                      putStrLn $ show Book ++ ": " ++ (show $ fst credits)
+                      putStrLn $ show CD   ++ ": " ++ (show $ snd credits)
       Nothing    -> putStrLn "Error: the argument must be a numeric number"
